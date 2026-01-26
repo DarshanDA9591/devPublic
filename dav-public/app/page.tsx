4235a2629cae50f4aa7c59d543c36c81be75dfd5
DARSHANDHANYAKU\Darshan DA <ddhanyakumara@urneeds.com>
2026-01-26 19:58:36 +0530

    Add global styles and layout components for the application

---
+++ dav-public/app/page.tsx
@@ -0,0 +1,15 @@
+import { Hero, About, Infrastructure, Academics, Gallery, Chatbot, Footer } from "../src/components";
+
+export default function Home() {
+  return (
+    <div className="bg-zinc-50 dark:bg-white font-sans">
+      <Hero />
+      <About />
+      <Infrastructure />
+      <Academics />
+      <Gallery />
+      <Chatbot />
+      <Footer />
+    </div>
+  );
+}
